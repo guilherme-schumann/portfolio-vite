2c9fbc49f3680df9acc37b9f47e13c0ddb7daff2
Guilherme Schumann <gui.rsc45@gmail.com>
2024-05-24 07:54:38 -0300

    adding menu mobile

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/contact.css';
 import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
